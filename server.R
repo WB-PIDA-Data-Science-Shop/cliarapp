088dce6f13b05ffb422fe721ec6091e5aab3f422
KANNAN VENKATARAMANAN <kannanv93@gmail.com>
2025-10-29 23:51:59 +0530

    Revert Temp Fix

--- server.R
+++ server.R
@@ -2847,20 +2847,20 @@ server <- function(input, output, session) {
           report = TRUE
         )
       
-      # plot2 <- data_dyn_avg() %>%
-      #   filter(str_detect(variable, "_avg"))%>%
-      #   static_plot_dyn(
-      #     base_country()[1],
-      #     "Country overview",
-      #     input$rank,
-      #     dots = input$benchmark_dots,
-      #     group_median = input$benchmark_median,
-      #     custom_df = custom_df,
-      #     threshold = input$threshold,
-      #     title = FALSE,
-      #   )
+      plot2 <- data_dyn_avg() %>%
+        filter(str_detect(variable, "_avg"))%>%
+        static_plot_dyn(
+          base_country()[1],
+          "Country overview",
+          input$rank,
+          dots = input$benchmark_dots,
+          group_median = input$benchmark_median,
+          custom_df = custom_df,
+          threshold = input$threshold,
+          title = FALSE,
+        )
       plot1 <- dml(ggobj = plot1)
-      #plot2 <- dml(ggobj = plot2)
+      plot2 <- dml(ggobj = plot2)
       
       table_data <- data.frame(
         Group = c("Base Country","Comparison Countries"),
@@ -2885,61 +2885,61 @@ server <- function(input, output, session) {
         ))
       
       
-      # slide_index = 10
-      # 
-      # #family_n <- data()%>%
-      # family_n <- db_variables%>%
-      #   distinct(family_name)%>%
-      #   filter(!is.na(family_name))%>%
-      #   pull(family_name) %>%
-      #   as.list()
-      # 
-      # 
-      # for(fam_n in family_order$family_name){
-      #   if(fam_n %in% family_n){
-      #     fam_variable_names<-variable_names %>%
-      #       filter(family_name == fam_n) %>%
-      #       pull(variable) %>%
-      #       unique()
-      #     
-      #     plt_f<-data_avg() %>%
-      #       filter(variable %in% fam_variable_names)%>%
-      #       static_plot(
-      #         base_country(),
-      #         fam_n,
-      #         input$rank,
-      #         dots = input$benchmark_dots,
-      #         group_median = input$benchmark_median,
-      #         custom_df = custom_df(),
-      #         threshold = input$threshold,
-      #         preset_order = input$preset_order,
-      #         title = FALSE,
-      #         report = TRUE
-      #       )
-      #     
-      #     plt_f<-dml(ggobj = plt_f)
-      #     
-      #     ppt <- ppt %>%
-      #       add_slide(master = "Custom Design")%>%
-      #       on_slide(index = slide_index) %>%
-      #       ph_with(value = fam_n, location = ph_location(left = 1, top = 0.4,width = 12))%>%
-      #       ph_with(value = plt_f, location = ph_location(
-      #         left = 1.5, top = 1.2,
-      #         width = 10.04, height = 4.67, bg = "transparent"
-      #       ))
-      #     
-      #     slide_index = slide_index+1
-      #   }
-      # }
+      slide_index = 10
+
+      #family_n <- data()%>%
+      family_n <- db_variables%>%
+        distinct(family_name)%>%
+        filter(!is.na(family_name))%>%
+        pull(family_name) %>%
+        as.list()
+
+
+      for(fam_n in family_order$family_name){
+        if(fam_n %in% family_n){
+          fam_variable_names<-variable_names %>%
+            filter(family_name == fam_n) %>%
+            pull(variable) %>%
+            unique()
+
+          plt_f<-data_avg() %>%
+            filter(variable %in% fam_variable_names)%>%
+            static_plot(
+              base_country(),
+              fam_n,
+              input$rank,
+              dots = input$benchmark_dots,
+              group_median = input$benchmark_median,
+              custom_df = custom_df(),
+              threshold = input$threshold,
+              preset_order = input$preset_order,
+              title = FALSE,
+              report = TRUE
+            )
+
+          plt_f<-dml(ggobj = plt_f)
+
+          ppt <- ppt %>%
+            add_slide(master = "Custom Design")%>%
+            on_slide(index = slide_index) %>%
+            ph_with(value = fam_n, location = ph_location(left = 1, top = 0.4,width = 12))%>%
+            ph_with(value = plt_f, location = ph_location(
+              left = 1.5, top = 1.2,
+              width = 10.04, height = 4.67, bg = "transparent"
+            ))
+
+          slide_index = slide_index+1
+        }
+      }
       
-      # ppt<-ppt%>%
-      #   add_slide(master = "Custom Design")%>%
-      #   on_slide(index = slide_index) %>%
-      #   ph_with(value = "Dynamic Benchmarking : Overview", location = ph_location(left = 1, top = 0.4,width = 12))%>%
-      #   ph_with(value = plot2, location = ph_location(
-      #     left = 1.5, top = 1.2,
-      #     width = 10.04, height = 4.67, bg = "transparent"
-      #   ))
+      ppt<-ppt%>%
+        add_slide(master = "Custom Design")%>%
+        on_slide(index = slide_index) %>%
+        ph_with(value = "Dynamic Benchmarking : Overview", location = ph_location(left = 1, top = 0.4,width = 12))%>%
+        ph_with(value = plot2, location = ph_location(
+          left = 1.5, top = 1.2,
+          width = 10.04, height = 4.67, bg = "transparent"
+        ))
       
       print(ppt, file)
     }
